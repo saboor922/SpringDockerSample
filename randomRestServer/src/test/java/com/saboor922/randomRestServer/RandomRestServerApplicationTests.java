04e366227bc480235e70bcccd18df4152b7092d9
saboor922 <abdussaboor2002@hotmail.com>
2019-02-05 11:37:17 +0000

    fixed RandomRestServerApplication Test + modified gitignore

--- randomRestServer/src/test/java/com/saboor922/randomRestServer/RandomRestServerApplicationTests.java
+++ randomRestServer/src/test/java/com/saboor922/randomRestServer/RandomRestServerApplicationTests.java
@@ -3,10 +3,12 @@ package com.saboor922.randomRestServer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(properties = { "randomUrl.path = http://randomUrl.com"})
 public class RandomRestServerApplicationTests {
 
 	@Test

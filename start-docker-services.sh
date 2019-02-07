282d9b282b67c44a4d12a94eb62df610ba4d24db
saboor922 <abdussaboor2002@hotmail.com>
2019-02-07 14:16:20 +0000

    Updated ENV variables in all dockerfiles to allow inter docker container communication

--- start-docker-services.sh
+++ start-docker-services.sh
@@ -6,5 +6,4 @@ docker-compose up -d config-service
 docker-compose up -d discovery-service
 sleep $sleep_seconds
 docker-compose up -d random-rest-service
-sleep $sleep_seconds
 docker-compose up -d vehicle-service
